207323004fa9acc8b92ad7eb9dc93194871013fe
Barış Bayazıt <bayazitbaris@gmail.com>
2024-05-08 15:30:54 +0300

    add MDX config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import remarkGfm from "remark-gfm"
+import createMDX from "@next/mdx"
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Include markdown and MDX files
+    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+    // Optionally, add any other Next.js config below
+}
+
+const withMDX = createMDX({
+    // Add markdown plugins here
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [],
+    },
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./content/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/mdx-components.tsx"
     ],
     theme: {
         extend: {
