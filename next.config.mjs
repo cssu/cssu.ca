9361edb16fd871bad5bec9366df9bfcee14fb669
Barış Bayazıt <bayazitbaris@gmail.com>
2024-05-09 23:24:59 +0300

    Copy images in webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,42 @@
 import remarkGfm from "remark-gfm"
 import remarkMdxImages from "remark-mdx-images"
 import createMDX from "@next/mdx"
+import path from "path"
+
+import CopyWebpackPlugin from "copy-webpack-plugin"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Include markdown and MDX files
+    // Accept MD and MDX files
     pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-    // Optionally, add any other Next.js config below
+    // Do not remove the unused parameters. They are explicitly stated here for clarity.
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+        config.plugins.push(
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.join(process.cwd(), "content", "**", "*.{png,jpeg,jpg}"),
+                        to: ({ context, absoluteFilename }) => {
+                            const relativePath = path.relative(
+                                path.join(process.cwd(), "content"),
+                                absoluteFilename
+                            )
+                            return path.join(process.cwd(), "public", "build-images", relativePath)
+                        },
+                        noErrorOnMissing: true,
+                        globOptions: {
+                            // DO NOT REMOVE! MacOS creates this hidden file, can be a burden when copying files.
+                            // Only extend the list with other files that are not needed, but don't remove anything
+                            // from the array below unless you have a very good reason to do so.
+                            ignore: ["**/.DS_Store"],
+                        },
+                    },
+                ],
+            })
+        )
+
+        return config
+    },
 }
 
 const withMDX = createMDX({
@@ -23,5 +53,4 @@ const withMDX = createMDX({
     },
 })
 
-// Merge MDX config with Next.js config
 export default withMDX(nextConfig)
