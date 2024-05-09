bfe5025065612cefcea1389c7d3c22dcc73e60fe
Barış Bayazıt <bayazitbaris@gmail.com>
2024-05-09 23:36:13 +0300

    remove shameful debug statement

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,7 +4,6 @@ import Image from "next/image"
 import type { MDXComponents } from "mdx/types"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-    console.log(components)
     return {
         a: ({ children, href }) =>
             href && (href.startsWith("/") || href.startsWith(".")) ? (
