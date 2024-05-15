c67582312e2d408b0184f2859aba828672bea005
Barış Bayazıt <bayazitbaris@gmail.com>
2024-05-15 23:39:41 +0300

    Change navbar text colour

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,7 +59,7 @@ function NavbarHref({ href, text, setMobileMenuIsOpen }: NavbarHrefProps) {
     return (
         <Link
             href={href}
-            className="text-[#999] block leading-normal relative px-3 py-2 grow-0 shrink-0 hover:text-black lg:items-center lg:flex"
+            className="text-gray-700 block leading-normal relative px-3 py-2 grow-0 shrink-0 hover:text-black lg:items-center lg:flex"
             onClick={() => setMobileMenuIsOpen(false)}
         >
             {text}
