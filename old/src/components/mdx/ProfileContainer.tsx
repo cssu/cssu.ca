3cd1f3f643a13d6c022f7f827501fa375380e5c9
YheChen <yanzhenchen123@gmail.com>
2025-04-04 18:08:43 -0400

    removed old files

--- old/src/components/mdx/ProfileContainer.tsx
+++
@@ -1,3 +0,0 @@
-export default function ProfileContainer({ children }: Readonly<{ children: React.ReactNode }>) {
-    return <div className="flex flex-wrap justify-center bg-white">{children}</div>
-}
--- old/src/lib/groupBy.ts
+++
@@ -1,7 +0,0 @@
-export default function groupBy<T>(array: T[], n: number): T[][] {
-    let groups = []
-    for (let i = 0; i < array.length; i += n) {
-        groups.push(array.slice(i, i + n))
-    }
-    return groups
-}
