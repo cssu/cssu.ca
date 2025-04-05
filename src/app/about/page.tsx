b164e7a05fd342e1616485badc3bf5ca92158ae2
YheChen <yanzhenchen123@gmail.com>
2025-04-04 21:25:29 -0400

    removed syntax error oopsie

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -64,7 +64,7 @@ export default function About() {
           Bahen Centre for Information Technology
         </a>{" "}
         or email us at <a href="mailto:executive@cssu.ca">executive@cssu.ca</a>.{" "}
-        <a href="/ba2250">See Office Hours.</a>q
+        <a href="/ba2250">See Office Hours.</a>
       </p>
 
       {/* Executive Council */}
